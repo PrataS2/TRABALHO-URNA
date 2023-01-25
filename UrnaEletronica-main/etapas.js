c439787bc4a202408c26715321eb3d6206200af2
Andrey <122835955+PrataS2@users.noreply.github.com>
2023-01-25 19:59:51 -0300

    Add files via upload

---
+++ UrnaEletronica-main/etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '1010',
+                nome: 'Oruan',
+                partido: 'Mainstreet',
+                fotos: [
+                    {url: '1.jpg', legenda: 'GOVERNADOR'}
+                ]
+            },
+            {
+                numero: '2020',
+                nome: 'TETO',
+                partido: '30PRAUM',
+                fotos: [
+                    {url: '2.jpg', legenda: 'GOVERNADOR'}
+                ]
+            },
+            
+        ]
+    },
+
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Mc Poze',
+                partido: 'Mainstreet',
+                vice: 'Cic',
+                fotos: [
+                    {url: '3.jpg', legenda: 'Prefeito'},
+                    {url: '4.jpg', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+            {
+                numero: '30',
+                nome: 'MATUE',
+                partido: '30PRAUM',
+                vice: 'Zul',
+                fotos: [
+                    {url: '5.jpg', legenda: 'Prefeito'},
+                    {url: '6.jpg', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+            
+        ]
+    }
+
+    
+    
+
+
+]
